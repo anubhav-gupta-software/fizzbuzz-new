17b3dbc2d53ef5ae906d700ea58c406ebeb56f0a
anubhav300 <anubhavgupta.main@gmail.com>
2024-05-16 06:41:54 +0530

    Basic Get Functionality Added

--- login_script.js
+++ login_script.js
@@ -1,10 +1,42 @@
-document.getElementById("login_button").addEventListener("click", gotClicked);
+function get(url) {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.onload = function() {
+        resolve({ status: http.status, data: JSON.parse(http.response) });
+      };
+      http.open("GET", url);
+      http.send();
+    });
+  }
+  
+function get_api_response(url) {
+    get(url).then(function(response){
+        let usernameApi = "";
+        let score = -1;
+        if (response.status == 200) {
+            usernameApi = response.data.id;
+            score = response.data.score;
+    }
+        else {
+            console.log("The Required Username Not Found");
+            console.log("Creating Username With Score Zero");
+            //post(url, {score: 0});
+        }
 
+    })
+}
+
+document.getElementById("login_button").addEventListener("click", gotClicked);  
 function gotClicked() {
     let usernameInput = document.getElementById("username").value;
     document.getElementById("invalid").textContent = "";
 
     if (usernameInput == "") {
-        document.getElementById("Invalid").textContent = "No Username Entered. Please Try Again!";
+        document.getElementById("invalid").textContent = "No Username Entered. Please Try Again!";
+    }
+    else {
+        let apiUrl = "http://localhost:8000/"
+        let requestUrl = apiUrl + usernameInput;
+        get_api_response(requestUrl)
     }
 }
