0590ecf9e7c3cc1d8535a5132cfd12ae1818933a
anubhav300 <anubhavgupta.main@gmail.com>
2024-05-16 11:53:10 +0530

    Added Proper Formatting

--- script_home.js
+++ script_home.js
@@ -1,52 +1,54 @@
 //Function to update/create data through API 
 function post(url, data) {
     data = JSON.stringify(data);
+
     return new Promise((resolve, reject) => {
-      const http = new XMLHttpRequest();
-      http.onload = function() {
-        resolve({ status: http.status, data: JSON.parse(http.response) });
-      };
-      http.open("POST", url);
-      http.setRequestHeader("Content-Type", "application/json");
-      http.send(data);
+        const http = new XMLHttpRequest();
+        http.onload = function() {
+            resolve({
+                status: http.status,
+                data: JSON.parse(http.response)
+            });
+        };
+
+        http.open("POST", url);
+        http.setRequestHeader("Content-Type", "application/json");
+        http.send(data);
     });
-  }
+}
 
 //Implementation of FizzBuzz checker
 function fizzbuzz_checker(score) {
     if (score % 3 == 0 && score % 5 == 0) {
         return ("FizzBuzz")
-    }
-    else if (score % 3 == 0){
+    } else if (score % 3 == 0) {
         return ("Fizz")
-    }
-    else if (score % 5 == 0){
+    } else if (score % 5 == 0) {
         return ("Buzz")
-    }
-    else {
+    } else {
         return score;
     }
 }
-//
-document.addEventListener("DOMContentLoaded", function(){//runs after HTML is parsed
+
+document.addEventListener("DOMContentLoaded", function() { //runs after HTML is parsed
     let apiUrl = "http://localhost:8000/"
-    
-    let username = sessionStorage.getItem("username_login");//accessing session storage variables
+    let username = sessionStorage.getItem("username_login"); //accessing session storage variables
     let requestUrl = apiUrl + username;
     let score = parseInt(sessionStorage.getItem("score_login"))
+
     // console.log(username)
     // console.log(score)
-    document.getElementById("username_home").textContent=username;//setting default or previous data
-    document.getElementById("score_home").textContent=fizzbuzz_checker(score);
 
-    document.getElementById("increment").addEventListener("click", gotClicked);  
-    
-    function gotClicked(){
-        score+=1;//incrementing score on each click
-        post(requestUrl, {score:score}).then(function(){//running after the score is updated in the server
-            document.getElementById("score_home").textContent=fizzbuzz_checker(score);
+    document.getElementById("username_home").textContent = username; //setting default or previous data
+    document.getElementById("score_home").textContent = fizzbuzz_checker(score);
+    document.getElementById("increment").addEventListener("click", gotClicked); //event listener for increment
+
+    function gotClicked() {
+        score += 1; //incrementing score on each click
+        post(requestUrl, {
+            score: score
+        }).then(function() { //running after the score is updated in the server
+            document.getElementById("score_home").textContent = fizzbuzz_checker(score);
         });
-        
     }
-    
 });
--- script_login.js
+++ script_login.js
@@ -1,66 +1,75 @@
 //Function to get data from API calls
 function get(url) {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
       const http = new XMLHttpRequest();
       http.onload = function() {
-        resolve({ status: http.status, data: JSON.parse(http.response) });
+          resolve({
+              status: http.status,
+              data: JSON.parse(http.response)
+          });
       };
+
       http.open("GET", url);
       http.send();
-    });
-  }
+  });
+}
 //Function to update/create data through API 
-  function post(url, data) {
-    data = JSON.stringify(data);
-    return new Promise((resolve, reject) => {
+function post(url, data) {
+  data = JSON.stringify(data);
+  return new Promise((resolve, reject) => {
       const http = new XMLHttpRequest();
       http.onload = function() {
-        resolve({ status: http.status, data: JSON.parse(http.response) });
+          resolve({
+              status: http.status,
+              data: JSON.parse(http.response)
+          });
       };
       http.open("POST", url);
       http.setRequestHeader("Content-Type", "application/json");
       http.send(data);
-    });
-  }
-  
-//Function to handle get and post requests
-function get_api_response(url) {
-    get(url).then(function(response){
-         if (response.status == 200) {
-            usernameApi = response.data.id;
-            score = response.data.score;
-            // console.log("The Username \"" + usernameApi + "\" is present.")
-            // console.log("The score is " + score)
-            sessionStorage.setItem('username_login', usernameApi)
-            sessionStorage.setItem('score_login', score) //session storage for data access
-            window.location.assign('home.html'); 
-          }
-        else {
-            // console.log("The Required Username Not Found, Creating User");
-            post(url, {score: 0}).then(function(){//creation of user with score 0
-              get_api_response(url)
-            });
-            
-        }
-      
-          
-
-    })
+  });
 }
 
-document.getElementById("login_button").addEventListener("click", gotClicked);//Login Button Click Event Listener
-function gotClicked() {
-    let usernameInput = document.getElementById("username").value;
-    document.getElementById("invalid").textContent = "";
+//Function to handle get and post requests
+function get_api_response(url) {
+  get(url).then(function(response) {
+      if (response.status == 200) {
+          usernameApi = response.data.id;
+          score = response.data.score;
+          // console.log("The Username \"" + usernameApi + "\" is present.")
+          // console.log("The score is " + score)
+          sessionStorage.setItem('username_login', usernameApi)
+          sessionStorage.setItem('score_login', score) //session storage for data access
+          window.location.assign('home.html');
+      } else {
+          // console.log("The Required Username Not Found, Creating User");
+          post(url, {
+              score: 0
+          }).then(function() { //creation of user with score 0
+              get_api_response(url)
+          });
 
-    if (usernameInput == "") {
-        document.getElementById("invalid").textContent = "No Username Entered. Please Try Again!";
-    }
-    else {//only runs when the username is non-null
-        let apiUrl = "http://localhost:8000/" //server address
-        let requestUrl = apiUrl + usernameInput;
-        get_api_response(requestUrl)
-        
-        
-    }
+      }
+
+
+
+  })
+}
+
+document.getElementById("login_button").addEventListener("click", gotClicked); //Login Button Click Event Listener
+
+function gotClicked() {
+  let usernameInput = document.getElementById("username").value;
+  document.getElementById("invalid").textContent = "";
+
+  if (usernameInput == "") {
+      document.getElementById("invalid").textContent = "No Username Entered. Please Try Again!";
+  } else {
+      //only runs when the username is non-null
+      let apiUrl = "http://localhost:8000/" //server address
+      let requestUrl = apiUrl + usernameInput;
+      get_api_response(requestUrl)
+
+
+  }
 }
